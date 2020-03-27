2bd19507037ec06796c1854e55f8cd5185fed60c
Vytautas <vytautasragaisis07@gmail.com>
2020-03-27 20:41:29 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,22 +5,31 @@ import uuidv4 from 'uuid/v4';
 
 function App() {
   const [recipes, setRecipes] = useState(sampleRecipes)
-  return (
-      <RecipeList recipes={sampleRecipes}/>
-  )
-}
-
-function handleRecipeAdd() {
-  const newRecipe= {
-    id: uuidv4(),
-    name: 'New',
-    servings: 1,
-    cookTime: '1:00',
-    instructions: 'Instr.',
-    ingredients: [
-      {id: uuidv4(), name: 'Name', amount: '1'}
-    ]
+  function handleRecipeAdd() {
+    const newRecipe= {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [
+        {id: uuidv4(), name: 'Name', amount: '1 Tbs'}
+      ]
+    }
+    setRecipes([...recipes, newRecipe])
   }
+  
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter(recipe => recipe.id !== id))
+  }
+  
+  return (
+      <RecipeList
+          recipes={recipes}
+          handleRecipeAdd={handleRecipeAdd}
+          handleRecipeDelete={handleRecipeDelete}
+      />
+  )
 }
 
 const sampleRecipes = [
